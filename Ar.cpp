7cd061cab4f87e7e2ef075f801a595ce6060ebb1
vodono <vdn2004@gmail.com>
2017-12-23 23:36:24 +0200

    [dev]Add Ar file

---
+++ Ar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by denysvysochanskyy on 23.12.17.
+//
+
+#include "Ar.h"
---
+++ Ar.h
@@ -0,0 +1,14 @@
+//
+// Created by denysvysochanskyy on 23.12.17.
+//
+
+#ifndef PROJECT_AR_H
+#define PROJECT_AR_H
+
+
+class Ar {
+
+};
+
+
+#endif //PROJECT_AR_H
